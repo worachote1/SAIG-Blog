feb7dd7825513c7eb7d70c8507dab4301e403235
worachote1 <timeworachote2002@gmail.com>
2022-09-25 23:03:49 +0700

    set State for Form Component e22

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -37,20 +37,35 @@ const NavBar = () => {
             </div>
 
             {/* Button */}
-            <div>{(true) &&
-                <button className='hidden
+            <div>
+
+                {/* Go create page */}
+                {(true) && <a href='/create'>
+                    <button className='hidden
+                px-7 py-3 bg-transparent rounded-full text-black font-medium text-sm leading-snug uppercase shadow-md hover:bg-teal-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+             md:inline-block'>
+                        Write
+                    </button>
+                    </a>
+                }
+                {/* Login */}
+                {(true) && <a>
+                    <button className='hidden
                 px-7 py-3 bg-transparent rounded-full text-black font-medium text-sm leading-snug uppercase shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
              md:inline-block'>
-                    Log in
-                </button>
-            }
+                        Log in
+                    </button>
+                    </a>
+                }
 
-                {(true) &&
+                {/* Logout */}
+                {(true) && <a>
                     <button className='hidden
                     "px-7 py-3 bg-transparent text-black rounded-full font-medium text-sm leading-snug uppercase  shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
              md:inline-block'>
                         Log out
                     </button>
+                    </a>
                 }
             </div>
 
@@ -73,7 +88,9 @@ const NavBar = () => {
                 <AiOutlineClose size={30} className="absolute top-4 right-2 cursor-pointer" onClick={() => setNav(!nav)} />
 
                 <h2 className='text-2xl p-4'>
+                    <a href='/'>
                     <span className='text-teal-400'>S A I G</span> B l o g
+                    </a>
                 </h2>
                 <nav>
                     <ul className='flex flex-col p-4  text-gray-800'>
@@ -81,22 +98,34 @@ const NavBar = () => {
                         <li className='text-xl py-4 flex items-center'><MdHelp size={25} className='mr-4' /> Help</li>
                         <li className='text-xl py-4 flex items-center'><FaUserFriends size={25} className='mr-4' /> Invite Friends</li>
                         <li>
-                            {(true) &&
+                            {(true) && <a href='/create'>
+                                <button className='
+                px-7 py-3 bg-transparent rounded-full text-black font-medium text-sm leading-snug uppercase shadow-md hover:bg-teal-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+             md:inline-block'>
+                                    Write
+                                </button>
+                                </a>
+                            }
+                        </li>
+                        <li>
+                            {(true) && <a>
                                 <button className='
                 px-7 py-3 bg-transparent rounded-full text-black font-medium text-sm leading-snug uppercase shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"'>
                                     Log in
                                 </button>
+                                </a>
                             }
                         </li>
 
                         <li>
-                            {(true) &&
+                            {(true) &&<a>
                                 <button className='
                     "px-7 py-3 bg-transparent text-black rounded-full font-medium text-sm leading-snug uppercase  shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"'>
                                     Log out
                                 </button>
+                                </a>
                             }
-
+                            
                         </li>
                     </ul>
                 </nav>
