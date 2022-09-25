86417455f970970c24e1811bfeefbf2d7b0da300
worachote1 <timeworachote2002@gmail.com>
2022-09-25 16:06:17 +0700

    NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/NavBar';
 import Login from './components/Login';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Login />
+  
+      <Footer />
     </div>
   );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,43 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai';
+import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs';
+import { TbTruckDelivery } from 'react-icons/tb'
+import { FaUserFriends, FaWallet } from 'react-icons/fa'
+import { MdFavorite, MdHelp } from 'react-icons/md'
 
 const NavBar = () => {
-  return (
-    
 
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-  <div class="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="" class="flex items-center">
-        <h1 className='text-2xl sm:text-3xl
-                    lg:text-4xl px-2'>
-            <span className='text-teal-400'>S A I G</span> B l o g
-        </h1>    
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
-      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-  )
+    const [nav, setNav] = useState(false)
+
+    return (
+
+        <div className='max-w-[1640px] mx-auto
+        flex justify-between items-center p-4 
+        '>
+            {/* left side */}
+            <div className='flex items-center'>
+                <div className='cursor-pointer'
+                    onClick={() => setNav(!nav)}>
+                    <AiOutlineMenu size={20.44} />
+                </div>
+                <h1 className='text-2xl sm:text-3xl
+                lg:text-4xl px-2'>
+                    <span className='text-teal-400'>S A I G</span> B l o g
+                </h1>
+
+            </div>
+
+            {/* Search Input */}
+            <div className='bg-gray-200 rounded-full flex items-center px-2
+            w-[200px] sm:w-[400px] lg:w-[500px]'>
+                <AiOutlineSearch size={20.44} />
+                <input className='p-2 focus:outline-none w-[100%] bg-transparent'
+                    type="text" placeholder='Search foods' />
+            </div>
+
+            {/* Cart Button */}
+            <div>{(true) &&
+                <button className='hidden
+                px-7 py-3 bg-transparent rounded-full text-black font-medium text-sm leading-snug uppercase shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+             md:inline-block'>
+                    Log in
+                </button>
+            }
+
+                {(true) &&
+                    <button className='hidden
+                    "px-7 py-3 bg-transparent text-black rounded-full font-medium text-sm leading-snug uppercase  shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+             md:inline-block'>
+                        Log out
+                    </button>
+                }
+            </div>
+
+
+            {/* Mobile Menu */}
+            {/* background overlay when click tab icon on the top left side? */}
+
+            {nav ?
+                <div className='bg-black/80 
+                fixed w-screen h-screen
+                top-0 left-0
+                z-10'>
+                </div>
+                : ""}
+
+            {/* if nav is set to false display the same , if it' true move nav side menu form left -100%  */}
+            <div className={nav ? 'bg-white fixed top-0 left-0 w-[300px] h-screen duration-300 z-20'
+                : 'bg-white fixed top-0 left-[-100%] w-[300px] h-screen duration-300 z-20'
+            }>
+                <AiOutlineClose size={30} className="absolute top-4 right-2 cursor-pointer" onClick={() => setNav(!nav)} />
+
+                <h2 className='text-2xl p-4'>
+                    <span className='text-teal-400'>S A I G</span> B l o g
+                </h2>
+                <nav>
+                    <ul className='flex flex-col p-4  text-gray-800'>
+                        <li className='text-xl py-4 flex items-center'><MdFavorite size={25} className='mr-4' /> Favorites</li>
+                        <li className='text-xl py-4 flex items-center'><MdHelp size={25} className='mr-4' /> Help</li>
+                        <li className='text-xl py-4 flex items-center'><FaUserFriends size={25} className='mr-4' /> Invite Friends</li>
+                        <li>
+                            <button className='hidden
+            md:flex items-center cursor-pointer bg-black text-white py-2 rounded-full'>
+                                <BsFillCartFill size={20} className='mr-2' />
+                                logout
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+
+    )
 }
 
 export default NavBar
